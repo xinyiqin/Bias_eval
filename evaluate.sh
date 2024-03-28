dd93938324f9368861c459bb8d528443f0a3df39
qinxinyi <qinxinyi@sensetime.com>
2024-03-29 05:33:03 +0800

    update

--- evaluate.sh
+++ evaluate.sh
@@ -1,7 +1,7 @@
 source global.sh
 # baichuan2-13b/internlm2-1-8b/internlm2-20b/llama-7b/llama-13b
 MODEL=gpt-4-turbo  #qwen-turbo/minimax/glm-4/sensechat/ernie/gpt-3.5-turbo-1106/gpt-3.5-turbo-0125
-DATASET=crowspairs #stereoset/bbq/crowspairs
+DATASET=stereoset #stereoset/bbq/crowspairs
 LANG=en #en/zh
 
 python evaluate.py \
